ce2b554d5283eee6dcb630aa2409abc6e4016c5f
Katia Velasquez <katia.velasquez@.com>
2023-03-13 10:47:27 -0300

    Subida de archivos

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyContext from "./myContext";
+import Navbar from "./components/Navbar";
+
+import Home from "./views/Home";
+import Favoritos from "./views/Favoritos";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const endpoint = "/fotos.json";
+  const [gallery, setGallery] = useState([]);
+  const [fav, setFav] = useState([]);
+  const [heart , setHeart] = useState({});
+  const globalState = { gallery , setGallery , fav , setFav , heart , setHeart}
+
+  const getData = async() => {
+    const response = await fetch(endpoint);
+    const photos = await response.json();
+    console.log(photos.photos);
+    const arrayPhotos = photos.photos;
+    setGallery(prevGallery => [...arrayPhotos]);
+    console.log(gallery);
+  }
+  useEffect(() => {
+    getData()
+  },[]);
+
+  return (
+    <div className="App">
+      <MyContext.Provider value = {globalState}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>
+    </div>
+  );
+}
